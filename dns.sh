3bdbb48e2f03b92601f084cbbfdf5ec430d61d4a
Pin-Yi-Chuchu <42373091+880831ian@users.noreply.github.com>
2020-11-10 08:47:05 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -16,23 +16,35 @@ domain='tw.yahoo.com'
 else
 domain="${2}"
 fi
+if [ -z "$3" ];then
+outfile='b.txt'
+else
+outfile="${3}"
+fi
 if [ "$1" == "help"  ];then
-	echo -e "Usage: sh $0 {ip address,help}"
+echo "---------------------------------------------" 
+	echo -e "用法: sh $0 {ip address,help} {domain} {output file}"
+	echo -e "* ip address 請輸入網段即可 Ex :(163.17.10)"
+	echo -e "* help 輸入後可查詢規則"
+	echo -e "* domain 預設值為tw.yahoo.com，輸入後可自行設定domain"
+	echo -e "* outfile 預設值為 b.txt，輸入後可自行設定outfile"
+echo "---------------------------------------------" 
 	exit
 elif [ -z "$1" ];then 
-	echo -e "${red}Please input IP address Ex :( 163.17.10 )or input help to view rules.${white}"
+	echo -e "${red}請輸入IP位址 Ex :( 163.17.10 )或是輸入help來查詢規則${white}"
 	exit 
 elif [ "${isip}" == "0" ];then
-	echo -e "${blue}IP address :${white}${green}$1${white}" | tee -a b.txt
-	echo -e "${blue}domain :${white}${green}${domain}${white}" | tee -a b.txt
+	echo -e "${blue}IP address :${white}${green}$1${white}" | tee -a $outfile
+	echo -e "${blue}domain :${white}${green}${domain}${white}" | tee -a $outfile
+	echo -e "${blue}outfile :${white}${green}${outfile}${white}" | tee -a $outfile
 else
-	echo -e "${red}The input is not IP address or help${white}"
+	echo -e "${red}輸入不是IP位址或是help，請重新輸入!${white}"
 	exit
 fi
-echo -e "${blue}scan open 53 port.....${white}"
+echo -e "${blue}掃描是否開啟 53 port.....${white}"
 nmap -sU -Pn -p 53 $1.* | grep -B3 '53/udp open  domain' | grep -o '[0-9\.]\{7,\}' > a.txt
 if [ -s "a.txt" ];then
-	echo -e "${blue}The following is open 53 port${white}"
+	echo -e "${blue}以下為有開啟 53 port ip address${white}"
 echo "---------------------------------------------" 
 		while read line;
 		do
@@ -44,17 +56,17 @@ echo "---------------------------------------------"
 		if [ -z $work ];then
 		echo "1" 1>/dev/null
 		else
-		echo $line >> b.txt 
+		echo $line >> $outfile
 		fi
 		done < a.txt
 else
-	echo -e "${red}53 port is not opened${white}" | tee -a b.txt
+	echo -e "${red}沒有開啟 53 port${white}" | tee -a $outfile
 fi
-echo "---------------------------------------------" | tee -a b.txt
-	echo -e "${blue}Output to b.txt${white}"
+echo "---------------------------------------------" | tee -a $outfile
+	echo -e "${blue}將檔案輸出至 ${outfile}${white}"
 #############################execution time###############################
 
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date="$starttime" +%s);
 end_seconds=$(date --date="$endtime" +%s);
-echo "Execution time：" $((end_seconds-start_seconds))"s";
+echo "執行時間：" $((end_seconds-start_seconds))"s";
